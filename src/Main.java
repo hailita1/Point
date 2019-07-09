0fc99441894683c72ee96ed313cf94a948e66765
= <=>
2019-07-09 11:32:47 +0700

    ahihi

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        Point2D point2D = new Point2D(2.0f, 4.0f);
+        String chuoi = point2D.toString();
+        System.out.println(chuoi);
+
+        Point3D point3D = new Point3D(2.0f, 4.0f, 6.0f);
+        String chuoi1 = point3D.toString();
+        System.out.println(chuoi1);
+    }
+}
---
+++ src/Point3D.java
@@ -0,0 +1,36 @@
+public class Point3D extends Point2D {
+    public float z = 0.0f;
+
+    public Point3D(float x, float y) {
+        super(x, y);
+    }
+
+    public Point3D(float x, float y, float z) {
+        super(x, y);
+        this.z = z;
+    }
+
+    public float getZ() {
+        return z;
+    }
+
+    public void setZ(float z) {
+        this.z = z;
+    }
+
+    public void setXYZ(float x, float y, float z) {
+        this.x = super.getX();
+        this.y = super.getY();
+        this.z = z;
+    }
+
+    public float[] getXYZ() {
+        float[] xyz = {super.getX(), super.getY(), this.z};
+        return xyz;
+    }
+
+    @Override
+    public String toString() {
+        return super.toString()+ "z=" + z + '}';
+    }
+}
